6ae6cbaf128db7c3b53dcc4e316fc6136564f992
Juan C Olmedo <doubleparked88@gmail.com>
2023-04-25 16:28:07 -0700

    temp files

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -15,6 +15,7 @@ class BestBooks extends React.Component {
     
     try{
       /* TODO: Make a GET request to your API to fetch all the books from the database  */
+      //http://localhost:3001/books
       let url = `${process.env.REACT_APP_SERVER}books`;
       let urlData = await axios.get(url);
       
